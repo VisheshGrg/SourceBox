e9cf5270f5d0f3327a45ae8bf5e34e1cab8c4fc7
Vishesh Garg <gargvishesh95885@gmail.com>
2024-05-17 18:01:53 +0530

    changed config

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const API_BASE_URL = "sourcebox-production.up.railway.app";
+// export const API_BASE_URL = "http://localhost:5454"
 
 const api = axios.create({baseURL:API_BASE_URL});
 
